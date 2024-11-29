c8579938235cbf40a56a312e67f680155574e105
davmendo <107776196+davmendo@users.noreply.github.com>
2024-11-29 00:13:34 +0000

    Update get_next_line_utils.c

--- testes/get_next_line_utils.c
+++ testes/get_next_line_utils.c
@@ -1,4 +1,26 @@
 #include "get_next_line.h"
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n--)
+		*ptr++ = (unsigned char) c;
+	return (s);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
 
 char	*ft_strdup(const char *s)
 {
@@ -10,17 +32,12 @@ char	*ft_strdup(const char *s)
 	{
 		i++;
 	}
-	dest = (char *)malloc((i + 1) * sizeof(char));
-	if (!dest)
-	{
-		return (NULL);
-	}
+	dest = (char *)ft_calloc((i + 1) * sizeof(char));
 	i = 0;
 	while (s[i])
 	{
 		dest[i] = s[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
